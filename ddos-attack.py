44be3a2ead4510d05bc46efa4cf920f51059f955
4lp3x <154073514+4lp3x@users.noreply.github.com>
2024-03-31 01:38:56 +0300

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,10 +20,10 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : Tech Tushar"
-print "You Tube : https://www.youtube.com/c/TechTushar"
-print "github   : https://github.com/Techtushar07"
-print "Facebook : https://www.facebook.com/daveratushar/"
+print "Yapımcı   : Alpex"
+print "You Tube : https://www.youtube.com/@AlpexTv"
+print "github   : https://github.com/4lp3x"
+print "Discord : a.l.p.e.x"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
